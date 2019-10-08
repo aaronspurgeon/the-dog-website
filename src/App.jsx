679602a848b2c7e59e2c93d58e0045ccdda57179
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-10-07 20:16:22 -0400

    Refactored old class component to functional component

--- src/App.jsx
+++ src/App.jsx
@@ -2,64 +2,70 @@ import React, {useState, useEffect} from 'react';
 import Axios from 'axios';
 import './App.css';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      breed: 'husky',
-      images: []
-    }
+function App(props) {
+
+  const [breed, setBreed] = useState('husky');
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    setImages([])
+    fetchDogImages()
+  }, [breed])
+
+  const handleChange = (e) => {
+    setBreed(e.target.value)
   }
 
-  componentDidMount() {
-    this.fetchDogImages()
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.breed !== this.state.breed) {
-      this.setState({
-        images: []
-      });
-      this.fetchDogImages()
-    }
-    
-  }
-
-  fetchDogImages = () => {
-    Axios.get(`https://dog.ceo/api/breed/${this.state.breed}/images`)
+  const fetchDogImages = () => {
+    Axios.get(`https://dog.ceo/api/breed/${breed}/images`)
       .then(res => {
-        this.setState({
-          images: res.data.message
-        })
+        setImages(res.data.message)
       })
       .catch(err => {
         console.log('Error: ', err)
       })
   }
-
-  handleChange = (e) => {
-    this.setState({
-      breed: e.target.value
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>dog website</h1>
-        <select value={this.state.breed} onChange={this.handleChange}>
-          <option disabled>Select breed</option>
-          <option value="husky">Husky</option>
-          <option value="beagle">Beagle</option>
-          <option value="corgi">Corgi</option>
-          <option value="boxer">Boxer</option>
-        </select>
-        {this.state.images.map((item, index) => (
-          <img src={item} key={index} alt="Dog"/>
-        ))}
-      </div>
-    )
-  }
+  return (
+    <div>
+      <h1>dog website</h1>
+      <select value={breed} onChange={handleChange}>
+        <option disabled>Select breed</option>
+        <option value="husky">Husky</option>
+        <option value="beagle">Beagle</option>
+        <option value="corgi">Corgi</option>
+        <option value="boxer">Boxer</option>
+      </select>
+      {images.map((item, index) => (
+        <img src={item} key={index} alt="Dog"/>
+      ))}
+    </div>
+  )
 }
+
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       breed: 'husky',
+//       images: []
+//     }
+//   }
+
+//   componentDidMount() {
+//     this.fetchDogImages()
+//   }
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (prevState.breed !== this.state.breed) {
+      
+      
+//     }
+    
+//   }
+
+  
+
+  
+// }
 
 export default App;
